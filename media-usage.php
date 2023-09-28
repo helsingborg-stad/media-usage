ce857c90c0d01baecfc0d8ff3f0e36981d428748
GitHub Action <>
2023-09-28 11:54:32 +0000

    build: update package file versions to 3.0.3

--- media-usage.php
+++ media-usage.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Media Usage
  * Plugin URI:        https://github.com/helsingborg-stad/media-usage
  * Description:       Track the use of attachments across posts, meta and options.
- * Version: 3.0.2
+ * Version: 3.0.3
  * Author:            Nikolas Ramstedt, Johan Silvergrund
  * Author URI:        https://github.com/helsingborg-stad
  * License:           MIT
